4a0be1bf1476973dfacf70256d3f3d003f462722
Eben Mullin <69456914+touchpotato@users.noreply.github.com>
2022-09-07 09:19:52 +1200

    Uploaded files

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const date = require(__dirname + "/date.js")
+
+const items = ["Buy 970 evo plus ssd", "Go for a drive", "Do some work on my website"];
+const workItems = [];
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+
+    const day = date.getDate();
+    
+    res.render("list", {listTitle: day, newListItems: items});
+        
+});
+
+app.post("/", function(req, res) {
+    const item = req.body.newItem;
+
+    if(req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work")
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+
+});
+
+app.get("/work", function(req, res) {
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res) {
+    res.render("about");
+})
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
---
+++ date.js
@@ -0,0 +1,26 @@
+//jshint esverstion:6
+
+exports.getDate = function() {
+
+    const today = new Date();
+        
+        const options = {
+            weekday: "long",
+            day: "numeric",
+            month: "long"
+        };
+        
+        return today.toLocaleDateString("en-US", options);
+}
+
+exports.getDay = function() {
+
+    const today = new Date();
+        
+        const options = {
+            weekday: "long" 
+        };
+        
+        return today.toLocaleDateString("en-US", options);
+
+}
